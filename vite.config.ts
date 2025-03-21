0880c10d49499636890b429bde6bc6e723b9a17a
leonardo_Wer <name89213126414@gmail.com>
2025-03-21 18:07:28 +0300

    Добавил алиасы в вайт

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import tsconfig from './tsconfig.json';
+
+// Путь к папке src
+const SRC_PATH = path.resolve(__dirname, 'src');
+
+// Функция для парсинга алиасов из tsconfig.json
+const parseTsConfigPaths = (paths: Record<string, string[]>): Record<string, string> => {
+  const viteAliases: Record<string, string> = {};
+
+  Object.entries(paths).forEach(([alias, paths]) => {
+    // Убираем звездочку (*) из пути
+    const aliasPath = paths[0].replace('/*', '');
+
+    // Создаем абсолютный путь
+    viteAliases[alias.replace('/*', '')] = path.join(SRC_PATH, aliasPath);
+  });
+
+  return viteAliases;
+};
+
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: parseTsConfigPaths(tsconfig.compilerOptions.paths),
+  },
 })
