98593d4846af54f42e089da0159dfff2f234fbaa
leonardo_Wer <name89213126414@gmail.com>
2025-03-21 19:49:36 +0300

    Создал базовые конфиги

---
+++ src/config/api.ts
@@ -0,0 +1,4 @@
+export const API_ENDPOINTS = {
+    USER_REQUEST: '/api/user',
+    LOGIN_REQUEST: '/api/login',
+}
---
+++ src/config/routes.ts
@@ -0,0 +1,8 @@
+export const ROUTES = {
+    MAIN: '/',
+    PAGE_1: '/page1',
+    PAGE_2: {
+        index: '/page2',
+        DETAILS: '/page2/details',
+    },
+}
